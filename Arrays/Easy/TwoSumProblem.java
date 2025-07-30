7dde6159033a787953e8dbf11f5b85022f4c8cbb
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-30 13:09:46 +0530

    LeetCode 1: Two Sum - Easy

--- Arrays/Easy/TwoSumProblem.java
+++ Arrays/Easy/TwoSumProblem.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i=0;i<nums.length-1;i++){
+
+        for (int i = 0; i < nums.length - 1; i++) {
             int first = nums[i];
-            for(int j=i+1;j<nums.length;j++){
+            for (int j = i + 1; j < nums.length; j++) {
                 int second = nums[j];
-                if(first+second == target)
-                    return new int[] {i,j};
-           }
+                if (first + second == target)
+                    return new int[] { i, j };
+            }
         }
         return null;
     }
