cbd60439bd783efeda944df4957322b9a78536c1
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-30 15:04:41 +0530

    LeetCode 189: Rotate Array - Medium

---
+++ Arrays/Hard/RotateArray.java
@@ -0,0 +1,25 @@
+package Arrays.Hard;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int size = nums.length-1;
+        int temp = 0;
+        k = k%nums.length;
+        for(int i=0;i<nums.length/2;i++){ // Step 1: Reversing the array
+            temp = nums[i];
+            nums[i] = nums[size-i];
+            nums[size-i] = temp;
+        }
+
+        for(int i=0;i<k/2;i++){ // Step 2: Reversing Left Half
+            temp = nums[i];
+            nums[i] = nums[k-i-1];
+            nums[k-i-1] = temp;
+        }
+        for(int i = k; i<(nums.length+k)/2;i++){ // Step 3:Reversing Right Half
+            temp = nums[i];
+            nums[i] = nums[size - (i-k)];
+            nums[size - (i-k)] = temp;
+        }
+    }
+}
