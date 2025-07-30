e81349c961d6b6622bbd5202ea25e06b74b2bbec
Jayakrishna <polakijayakrishna@gmail.com>
2025-07-30 13:00:28 +0530

    Add TwoSumProblem solution implementation

---
+++ Arrays/Easy/TwoSumProblem.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for(int i=0;i<nums.length-1;i++){
+            int first = nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                int second = nums[j];
+                if(first+second == target)
+                    return new int[] {i,j};
+           }
+        }
+        return null;
+    }
+}
