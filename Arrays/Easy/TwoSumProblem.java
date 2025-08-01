fc15fe4d23ffd0a852a27d12084ce0fedc814a6c
Jayakrishna <polakijayakrishna@gmail.com>
2025-08-01 10:59:56 +0530

    LeetCode 1: Two Sum - Easy

--- Arrays/Easy/TwoSumProblem.java
+++ Arrays/Easy/TwoSumProblem.java
@@ -1,4 +1,8 @@
-class Solution {
+import java.util.Map;
+import java.util.HashMap;
+
+//This is using array Approach  . Time Complexity O(N^2)
+/*class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length - 1; i++) {
@@ -10,5 +14,20 @@ class Solution {
             }
         }
         return null;
+    }
+}*/
+
+//This Solution is by using HashMap . -> Time Complexity is O(N)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff))
+                return new int[] {map.get(diff) , i};
+            else 
+                map.put(nums[i] , i);
+        }
+        return new int[] {0};
     }
 }
