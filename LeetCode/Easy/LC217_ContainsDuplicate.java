01cf3580c1b891c4704569ce897250dae6c449f4
Polaki Jaya Krishna <polakijayakrishna@gmail.com>
2025-11-16 09:23:40 +0530

    Update LC217 Contains Duplicate with proper class, package, and structure

--- LeetCode/Easy/LC217_ContainsDuplicate.java
+++ LeetCode/Easy/LC217_ContainsDuplicate.java
@@ -1,12 +1,23 @@
-class Solution {
+package LeetCode.Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LC217_ContainsDuplicate {
+
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for(int num:nums){
-            if(!set.contains(num))
-                set.add(num);
-            else 
-                return true;
+        for (int n : nums) {
+            if (!set.add(n)) {
+                return true;   // duplicate found
+            }
         }
-        return false;
+        return false;  // no duplicates
+    }
+
+    public static void main(String[] args) {
+        LC217_ContainsDuplicate obj = new LC217_ContainsDuplicate();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(obj.containsDuplicate(nums));
     }
 }
